5760998e1bcdf601d0b704cc5aca2caa232777fe
siddharth-fultariya <41518997+siddharth-fultariya@users.noreply.github.com>
2019-03-08 00:25:35 +0530

    Update server.java

--- Chat_app(swing)/server.java
+++ Chat_app(swing)/server.java
@@ -27,27 +27,12 @@ class Chat
 	volatile String str = "";
 	volatile String recipient;
 	volatile String client_name;
-		
-	
-	/*synchronized void write_chat() throws InterruptedException
-	{
-		if(str == "" || str.equals("change recipient"))
-		{
-			System.out.println("in write_chat");
-			wait();
-		}	
-	}
-	synchronized void read_user(DataInputStream dr) throws InterruptedException,IOException
-	{
-		str = dr.readUTF();
-		System.out.println("your name = "+client_name+"recipient  "+recipient+" message "+str);
-		notifyAll();
-	}*/
+			
 }
+
 class thread extends Thread
 {
-	Socket s0 = null;							//to communicate with intial client..dw0
-	//Socket s1;							//to communication with recipient client..dw1
+	Socket s0 = null;							//to communicate with intial client..dw0	
 	int client_number;
 	char read_write;
 	Record r;
@@ -60,8 +45,7 @@ class thread extends Thread
 	thread(Socket s,int number,char rw,Record r,Chat ch)
 	{
 		this.s0 = s;
-		client_number = number;
-		//System.out.println("constructor "+number);
+		client_number = number;		
 		read_write = rw;
 		this.ch = ch;
 		this.r = r;
@@ -69,7 +53,7 @@ class thread extends Thread
 	}
 	public void run()
 	{
-		//System.out.println("number...in .."+client_number+"socket.. "+s0);
+		
 		try
 		{
 			if(read_write=='w')
@@ -77,24 +61,13 @@ class thread extends Thread
 				
 				dw0 = new DataOutputStream(s0.getOutputStream());      // to know the name of client..
 				dw0.writeUTF("-- Enter your name to communicate with other.. \n\n -- thenEnter your recipient name with whom you to communicate.. \n\n -- to change recipient just write 'change recipient'..then specify his/her name");
-				//dw0.writeUTF("then Enter your recipient name with whom you to communicate.. ");
-				//dw0.writeUTF("to change recipient just write 'change recipient'..then specify his/her name");
+				
+				
 				while(true)
 				{
 					while(ch.str == "" || (ch.str).equals("change recipient"));
 					try
-					{
-						//ch.write_chat();
-						/*synchronized(this)
-						{
-							while(ch.str == "" || (ch.str).equals("change recipient"))
-							{	
-								System.out.println("in sync....");
-								wait();
-							}
-							System.out.println("in sync 2");
-						}*/	
-						//System.out.println("your name = "+ch.client_name+"recipient name "+ch.recipient+" message "+ch.str);
+					{												
 						recipient_socket = r.client_socket.get(r.client_name.indexOf(ch.recipient));
 						dw1 = new DataOutputStream(r.s[recipient_socket].getOutputStream());
 						r.jw[recipient_socket].write_to(dw1,ch.client_name,ch.str);
@@ -103,6 +76,7 @@ class thread extends Thread
 					}
 					
 					catch(Exception e){
+					
 						System.out.println("recipient may be offline..try another one..");
 						dw0.writeUTF("recipient may be offline..try another one..");
 						e.printStackTrace();
@@ -113,23 +87,13 @@ class thread extends Thread
 					{
 						ch.str = "";
 					}
-					/*if(recipient_socket == -1)
-					{
-						System.out.println("recipient may be offline..try another one..");
-						ch.str = "";
-						continue;
-					}*/
-					//dw1 = new DataOutputStream(r.s[recipient_socket].getOutputStream());
-					//dw1.writeUTF(ch.client_name);
-					//dw1.writeUTF(ch.str);
-					//ch.str = "";
+					
 				}	
 			}
 			else
 			{
 				dr = new DataInputStream(s0.getInputStream());
-				ch.client_name = dr.readUTF();
-				//System.out.println("your name = "+ch.client_name);
+				ch.client_name = dr.readUTF();				
 				r.client_name.add(ch.client_name);		
 				r.client_socket.add(client_number);
 				while(true)
@@ -138,52 +102,18 @@ class thread extends Thread
 					ch.str = dr.readUTF();
 					while(!((ch.str).equals("change recipient")))
 					{
-						ch.str = dr.readUTF();
-						//ch.read_user(dr);
+						ch.str = dr.readUTF();						
 					}	
 					
 				}	
-				//System.out.println("number....."+client_number);
+				
 			}
-			/*while(read_write == 'w' && ch.str == "")
-			{
-				//System.out.println("str "+ch.str);
-			}*/
-			/*if(read_write == 'w' && ch.str!="")
-			{
-				//System.out.println("inside write recipient is "+ch.recipient);
-				//System.out.println("index.........."+r.client_name.indexOf(ch.recipient));
-				
-				//System.out.println("re so "+recipient_socket);
-				
-				//System.out.println("writing a message at port s index "+r.client_name.indexOf(ch.recipient));
-				//synchronized(ch)
-				//{
-					
-				//}
-			}*/	
-			/*else
-			{
-				//synchronized(ch)
-				//{
-					
-					dr = new DataInputStream(s0.getInputStream());
-					//ch.str+=dr.readUTF();
-					System.out.println("Enter a recipient name with whom you want to communicate..");
-					ch.recipient = (String)dr.readUTF();
-					//System.out.println("recipient name is "+ch.recipient);
-					ch.str = dr.readUTF();
-					//System.out.println("str should be "+ch.str);		
-					//System.out.println("your data "+s0);
-					//System.out.println("recipient port "+ r.s[r.client_socket.get(r.client_name.indexOf(ch.recipient))]);
-					//ch.str = "Enter your recipient name with whom you want to communicate.. ";
-				//}	
-			}*/
+						
+			
 		}
 		catch(Exception e)
 		{
-			//e.printStackTrace();
-			//System.out.println("error...."+e.getCause());
+						
 			System.out.println("recipient may be offline..");
 			
 		}
@@ -216,15 +146,13 @@ class server
 		int client_count=0;
 		while(true)
 		{
-			//r.s[client_count] = null;
-			ch = new Chat();
-			//System.out.println("in main");
+		
+			ch = new Chat();		
 			r.s[client_count] = sc.accept();
 			System.out.println("server side write "+r.s[client_count]);
 			r.jw[client_count] = new just_write();
 			tr[client_count] = new thread(r.s[client_count],client_count,'r',r,ch);
-			tw[client_count] = new thread(r.s[client_count],client_count,'w',r,ch);	
-			
+			tw[client_count] = new thread(r.s[client_count],client_count,'w',r,ch);				
 			client_count++;
 		}
 	}
